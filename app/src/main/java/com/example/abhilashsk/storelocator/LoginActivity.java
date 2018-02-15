25cef24410ddd513d4f2b52c78b46ba2bfe5f895
abhilashsk <abhilashkulkarni340@hotmail.com>
2018-02-15 19:21:17 +0530

    created new activity

--- app/src/main/java/com/example/abhilashsk/storelocator/LoginActivity.java
+++ app/src/main/java/com/example/abhilashsk/storelocator/LoginActivity.java
@@ -1,13 +1,33 @@
 package com.example.abhilashsk.storelocator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String EXTRA_USERNAME = "com.example.abhilashsk.USERNAME";
+    public static final String EXTRA_PASSWORD = "com.example.abhilashsk.PASSWORD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DashboardActivity.class);
+        EditText username_text = (EditText) findViewById(R.id.username_login);
+        EditText password_text = (EditText) findViewById(R.id.password_login);
+        String username = username_text.getText().toString();
+        String password = password_text.getText().toString();
+
+        intent.putExtra(EXTRA_USERNAME,username);
+        intent.putExtra(EXTRA_PASSWORD,password);
+
+        startActivity(intent);
+
     }
 }
