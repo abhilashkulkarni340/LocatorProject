3d977bd7aac38415b4b07fd345c336808f3f6b42
abhilashsk <abhilashkulkarni340@hotmail.com>
2018-02-15 22:37:40 +0530

    updated dashboard

--- app/src/main/java/com/example/abhilashsk/storelocator/DashboardActivity.java
+++ app/src/main/java/com/example/abhilashsk/storelocator/DashboardActivity.java
@@ -1,7 +1,9 @@
 package com.example.abhilashsk.storelocator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra(LoginActivity.EXTRA_USERNAME);
+        String password = intent.getStringExtra(LoginActivity.EXTRA_PASSWORD);
+
+        TextView display = findViewById(R.id.display_text);
+        display.setText("WELCOME "+username);
     }
 }
